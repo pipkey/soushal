0128ff5ca2517486d328749bf15aead8fdd0a8aa
pipkey <galagen2014@gmail.com>
2021-06-23 14:42:28 +0300

    Updates

---
+++ precache-manifest.80a811ac1a0f258c83fc9e7fd103c1bc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f78e125434f4e72f5207e746d6dd80",
+    "url": "/soushal/index.html"
+  },
+  {
+    "revision": "18acdb317ae887b585f2",
+    "url": "/soushal/static/css/4.111549b8.chunk.css"
+  },
+  {
+    "revision": "36fcae85b0cc4ff64640",
+    "url": "/soushal/static/css/5.6bd4b574.chunk.css"
+  },
+  {
+    "revision": "0cfa864977812e83a982",
+    "url": "/soushal/static/css/6.a65905bc.chunk.css"
+  },
+  {
+    "revision": "13ead136a3141a4e5714",
+    "url": "/soushal/static/css/7.b8dff8ee.chunk.css"
+  },
+  {
+    "revision": "7fbd0b58a990a7aa3941",
+    "url": "/soushal/static/css/main.cc0796df.chunk.css"
+  },
+  {
+    "revision": "19929e5c8f5b0b049cff",
+    "url": "/soushal/static/js/0.cdf5a841.chunk.js"
+  },
+  {
+    "revision": "a8049da57fc0e7738759",
+    "url": "/soushal/static/js/3.1e823e31.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/soushal/static/js/3.1e823e31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18acdb317ae887b585f2",
+    "url": "/soushal/static/js/4.e3918886.chunk.js"
+  },
+  {
+    "revision": "36fcae85b0cc4ff64640",
+    "url": "/soushal/static/js/5.07289967.chunk.js"
+  },
+  {
+    "revision": "0cfa864977812e83a982",
+    "url": "/soushal/static/js/6.601253a6.chunk.js"
+  },
+  {
+    "revision": "13ead136a3141a4e5714",
+    "url": "/soushal/static/js/7.1d054a76.chunk.js"
+  },
+  {
+    "revision": "7fbd0b58a990a7aa3941",
+    "url": "/soushal/static/js/main.5b25a45d.chunk.js"
+  },
+  {
+    "revision": "bf10b535d4fe86f9806e",
+    "url": "/soushal/static/js/runtime-main.bbe2d44f.js"
+  },
+  {
+    "revision": "272baf8c6b8a656e65ae24e7013edcd2",
+    "url": "/soushal/static/media/grid.272baf8c.svg"
+  },
+  {
+    "revision": "ca1bd2f63cccc4277eb27cfc641cb855",
+    "url": "/soushal/static/media/user.png.ca1bd2f6.png"
+  }
+]);
